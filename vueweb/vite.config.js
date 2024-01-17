6029e344ac60314ec2dd55c93c4b112213e68414
pang <11016205+p-xh@user.noreply.gitee.com>
2024-01-17 10:27:32 +0800

    1`

--- vueweb/vite.config.js
+++ vueweb/vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'vueb/dist/',
+  base: 'vueweb/dist/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
