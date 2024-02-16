598ee9cdb540c18e052263a988a7e19408420799
pang <11016205+p-xh@user.noreply.gitee.com>
2024-02-16 19:23:14 +0800

    11

--- vueweb/src/App.vue
+++ vueweb/src/App.vue
@@ -2,6 +2,12 @@
   <WeB></WeB>
   <RouterView class="ssdss"></RouterView>
   <FooT></FooT>
+  <div class="ssdss">
+    <div class="ssdss" v-for="i in 10000000" :key="i">
+      <p>{{ i }}</p>
+      <p>{{ i + 1 }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
